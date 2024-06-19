980e54601b18739deaadcf5009b537fbfcc4845f
San Valen <osoriovalentin5@gmail.com>
2024-06-19 13:35:54 -0500

    Add Microservice config

---
+++ src/config/envs.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+interface EnvVars {
+  // PORT: number;
+  // DATABASE_URL: string;
+  NATS_SERVERS: string[];
+}
+
+const envsSchema = z
+  .object({
+    // PORT: z.coerce.number(),
+    // DATABASE_URL: z.coerce.string(),
+    NATS_SERVERS: z.array(z.coerce.string()),
+  })
+  .passthrough();
+
+const envVars = envsSchema.parse({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
+
+export const envs: EnvVars = {
+  ...envVars,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3004);
+  const logger = new Logger('Auth-Main');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.NATS_SERVERS,
+      },
+    },
+  );
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  await app.listen();
+  logger.log(`Auth microservice running`);
 }
 bootstrap();
